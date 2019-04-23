6141e8be4ceca096df8af2bc41c0e9dc92eaa8be
hwwhwww <1907375750@qq.com>
2019-04-23 21:11:08 +0800

    ww

--- demo/src/main/java/com/xmxjc/demo/DemoApplication.java
+++ demo/src/main/java/com/xmxjc/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("我的第一个分支");
+        System.out.println(123);
         SpringApplication.run(DemoApplication.class, args);
     }
 
